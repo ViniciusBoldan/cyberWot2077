347ec016c2bdda3ba40f6a232303db9b57339776
ViniciusBoldan <74440733+ViniciusBoldan@users.noreply.github.com>
2021-01-25 16:22:51 -0400

    Update to 1.2
    Agora todos os botões com tanques funcionam, e existe um inimigo a ser destruido. Para jogas, basta inserir o angulo de normalização, a espessura da blindagem do inimigo, a angulo do disparo, e a distancia de 1 a 10. Pra fazer o disparo, basta clicar num dos tanques, e o codigo calcula as chances de acerto, a penetração,e o dano caso haja penetração.

--- cyberWot2077.js
+++ cyberWot2077.js
@@ -1,19 +1,36 @@
-function myTank(hp,armor, calibre, penAp, penAPCR){
+let shots = 1
+function myTank(hp,armor, calibre, penAP, penAPCR, dmg){
 
-    
     let projetil = Number(document.getElementById('projetil').value)
-    let  caliber = calibre
     let bliInimiga = Number(document.getElementById('bliInimiga').value)
     let angDisparo = Number(document.getElementById('angDisparo').value)
+    let distancia = Number(document.getElementById('distancia').value)
 
-    let alea = Math.round(Math.random()*90)+16
-    console.log(alea)
-    
-    if(alea <= 0){
-        console.log(`${alea}% De chances de acerto. Errou disparo.`)
-    }else{
-        let blindagemRelativa= bliInimiga / (Math.cos(angDisparo-(projetil* 1.4*caliber)/bliInimiga))
-        console.log(calibre)
-        console.log(`Penetração: ${blindagemRelativa}`)
+    let blindagemRelativa= bliInimiga / Math.cos(angDisparo*(projetil* 1.4*calibre)/bliInimiga)
+
+    if(blindagemRelativa < 0){
+         blindagemRelativa = Math.abs(blindagemRelativa)
     }
+
+    console.log(`${shots++}° Disparo`)
+    console.log(`Blindagem Relativa: ${blindagemRelativa}`)
+
+    let alea = Math.round(Math.random()*90)+16 - distancia
+
+    if(alea <= 49){
+        if(alea > 39 || alea < 50){
+            console.log(`Essa foi pro pouco`)
+        }else{
+            console.log(`Não acertamos o inimigo. ${alea}% de chances de acerto`)
+        }
+    }else{
+        console.log(`${alea}% De chances de acerto. Acertamos o Inimigo`)
+
+        if(blindagemRelativa < penAPCR){
+            let newHP = hp - dmg
+        console.log(`Penetramos a blindagem inimiga. Hp do inimigo: ${newHP}/${hp}`)
+    }else{
+        console.log(`Não penetramso a blindagem inimiga`)
+    }
+}
 }
